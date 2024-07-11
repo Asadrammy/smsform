66ae3870079ef4d2c222ba5e0fda3833f071827b
Asadrammy <asadrammy9594@gmail.com>
2024-07-11 14:21:58 +0500

    Add files via upload

--- edit.php
+++ edit.php
@@ -6,39 +6,37 @@ $server ="localhost";
 $username="root";
 $password="";
 $db="sms";
-$grade_id=$_GET['grade_id'];
+$notification_id=$_GET['notification_id'];
 $con= mysqli_connect($server, $username, $password,$db);
 /*if($con){
 echo "connected to db";}
 else{
    echo"not connected to db";}*/
-   $slc=" select * from grades where grade_id='$grade_id'";
+   $slc=" select * from notifications where notification_id='$notification_id'";
    $result=mysqli_query($con,$slc);
    while($row=mysqli_fetch_assoc($result)){
-    $grade_id=$row['grade_id'];
+    $notification_id=$row['notification_id'];
 
-    $student_id=$row['student_id'];
-    $subject_id=$row['subject_id'];
-    $grade=$row['grade'];
-    $date_recorded=$row['date_recorded'];
+    $user_id=$row['user_id'];
+    $message=$row['message'];
+    $date_sent=$row['date_sent'];
+    
   
    }
    ?>
 
-<form action="update.php" method="POST" id="gradesForm">
-<input type="hidden" id="student_id" name="grade_id" value="<?php echo $grade_id;?>"><br><br>
-    <label for="student_id">Student ID:</label>
-    <input type="text" id="student_id" name="student_id" value="<?php echo $student_id;?>"><br><br>
+<form action="update.php" method="POST">
+    <h2>Notification</h2>
+    <input type="hidden"  name="notification_id"  value="<?php echo $notification_id;?>"><br><br>
+    <label for="student_id">user ID:</label>
+    <input type="id"  name="user_id"  value="<?php echo $user_id;?>"><br><br>
 
-    <label for="subject_id">Subject ID:</label>
-    <input type="text" id="subject_id" name="subject_id" value="<?php echo $subject_id;?>"><br><br>
+    <label for="amount">Message:</label>
+    <input type="text" name="message" value="<?php echo $message;?>"><br><br>
 
-    <label for="grade">Grade:</label>
-    <input type="text" id="grade" name="grade" value="<?php echo $grade;?>"><br><br>
-<br><br>
-    <label for="date_recorded">Date Recorded:</label>
-    <input type="date" id="date_recorded" name="date_recorded" value="<?php echo $date_recorded;?>"><br><br>
+    <label for="due_date">date sent:</label>
+    <input type="date"  name="date_sent"  value="<?php echo $date_sent;?>"><br><br>
 
-<br><br>
+    
     <input type="submit" value="Submit">
 </form>
